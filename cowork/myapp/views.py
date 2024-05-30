37774f318270c5d1514f784a999fe41b1658334c
yjbae3974 <brianbae6@gmail.com>
2024-05-30 20:44:27 +0900

    completed list view

--- cowork/myapp/views.py
+++ cowork/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+def list(request):
+    posts = Post.objects.all()
+    return render(request, 'list.html', {'posts': posts})
+
