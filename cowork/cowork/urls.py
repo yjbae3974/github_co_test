40cefdc05d246441d28a9e9bac411e648ee7725c
Jeongjoshua <Jeongjoshua@outlook.com>
2024-05-30 20:43:02 +0900

    add details logic

--- cowork/cowork/urls.py
+++ cowork/cowork/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('detail/<int:post_pk>', views.detail, name='detail'),
 ]
--- cowork/myapp/views.py
+++ cowork/myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from myapp import models
 # Create your views here.
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    
+    return render(request, 'detail.html', {'post':post})
